f50a44a090d606f6bb5bf594b60dea1102cabfad
Alex Stewart <foogod@gmail.com>
2014-02-04 22:55:22 -0800

    Added many constants and typedefs Added add/sub/mul/div methods for btVector3 and friends Added btSerializer.getBuffer()

--- cppyy/cppyy_bullet.h
+++ cppyy/cppyy_bullet.h
@@ -28,4 +28,42 @@ template class btAxisSweep3Internal<unsigned short>::Handle;
 
 // Macros/Constants we want to make available to the Python side
 
-static btScalar _py_BT_LARGE_FLOAT = BT_LARGE_FLOAT;
+#ifdef BT_NO_PROFILE
+    static const bool _py_BT_NO_PROFILE = true;
+#else
+    static const bool _py_BT_NO_PROFILE = false;
+#endif
+#ifdef USE_BT_CLOCK
+    static const bool _py_USE_BT_CLOCK = true;
+#else
+    static const bool _py_USE_BT_CLOCK = false;
+#endif
+
+static const btScalar _py_BT_LARGE_FLOAT = BT_LARGE_FLOAT;
+
+static const btScalar _py_SIMD_2_PI = SIMD_2_PI;
+static const btScalar _py_SIMD_PI = SIMD_PI;
+static const btScalar _py_SIMD_HALF_PI = SIMD_HALF_PI;
+static const btScalar _py_SIMD_RADS_PER_DEG = SIMD_RADS_PER_DEG;
+static const btScalar _py_SIMD_DEGS_PER_RAD = SIMD_DEGS_PER_RAD;
+static const btScalar _py_SIMDSQRT12 = SIMDSQRT12;
+static const btScalar _py_SIMD_EPSILON = SIMD_EPSILON;
+static const btScalar _py_SIMD_INFINITY = SIMD_INFINITY;
+
+static const int _py_ACTIVE_TAG = ACTIVE_TAG;
+static const int _py_ISLAND_SLEEPING = ISLAND_SLEEPING;
+static const int _py_WANTS_DEACTIVATION = WANTS_DEACTIVATION;
+static const int _py_DISABLE_DEACTIVATION = DISABLE_DEACTIVATION;
+static const int _py_DISABLE_SIMULATION = DISABLE_SIMULATION;
+
+static const int _py_BT_6DOF_FLAGS_AXIS_SHIFT = BT_6DOF_FLAGS_AXIS_SHIFT;
+
+static const int _py_TRI_INFO_V0V1_CONVEX = TRI_INFO_V0V1_CONVEX;
+static const int _py_TRI_INFO_V1V2_CONVEX = TRI_INFO_V1V2_CONVEX;
+static const int _py_TRI_INFO_V2V0_CONVEX = TRI_INFO_V2V0_CONVEX;
+static const int _py_TRI_INFO_V0V1_SWAP_NORMALB = TRI_INFO_V0V1_SWAP_NORMALB;
+static const int _py_TRI_INFO_V1V2_SWAP_NORMALB = TRI_INFO_V1V2_SWAP_NORMALB;
+static const int _py_TRI_INFO_V2V0_SWAP_NORMALB = TRI_INFO_V2V0_SWAP_NORMALB;
+
+static const btScalar _py_CONVEX_DISTANCE_MARGIN = CONVEX_DISTANCE_MARGIN;
+
--- python/bullet/fixups.py
+++ python/bullet/fixups.py
@@ -6,15 +6,42 @@
 # called in __init__.py
 
 import cppyy
+import cffi
+
+ffi = cffi.FFI()
+
+# Extra operator functions which should be added to some classes:
+
+cppyy.gbl.btVector3.__mul__ = getattr(cppyy.gbl, 'operator*')
+cppyy.gbl.btVector3.__rmul__ = getattr(cppyy.gbl, 'operator*')
+cppyy.gbl.btVector3.__add__ = getattr(cppyy.gbl, 'operator+')
+cppyy.gbl.btVector3.__radd__ = getattr(cppyy.gbl, 'operator+')
+cppyy.gbl.btVector3.__sub__ = getattr(cppyy.gbl, 'operator-')
+cppyy.gbl.btVector3.__neg__ = getattr(cppyy.gbl, 'operator-')
+cppyy.gbl.btVector3.__div__ = getattr(cppyy.gbl, 'operator/')
+
+cppyy.gbl.btMatrix3x3.__mul__ = getattr(cppyy.gbl, 'operator*')
+cppyy.gbl.btMatrix3x3.__rmul__ = getattr(cppyy.gbl, 'operator*')
+cppyy.gbl.btMatrix3x3.__add__ = getattr(cppyy.gbl, 'operator+')
+cppyy.gbl.btMatrix3x3.__radd__ = getattr(cppyy.gbl, 'operator+')
+cppyy.gbl.btMatrix3x3.__sub__ = getattr(cppyy.gbl, 'operator-')
+
+cppyy.gbl.btQuaternion.__mul__ = getattr(cppyy.gbl, 'operator*')
+cppyy.gbl.btQuaternion.__rmul__ = getattr(cppyy.gbl, 'operator*')
+
+# Misc overrides for particular methods:
 
 def getOpenGLMatrix(self, matrix=None):
     if matrix is None:
         return list(cppyy.gbl._py_OpenGLMatrix(self).m_matrix)
     else:
-        m = cppyy.gbl._py_OpenGLMatrix(self)
-        del matrix[:]
-        matrix.extend(m.m_matrix)
+        matrix[:] = cppyy.gbl._py_OpenGLMatrix(self).m_matrix
         return matrix
 
 cppyy.gbl.btTransform.getOpenGLMatrix = getOpenGLMatrix
 
+def getBuffer(self):
+    cdata = ffi.cast("char *", self.getBufferPointer().buffer)
+    return ffi.buffer(cdata, self.getCurrentBufferSize())
+
+cppyy.gbl.btSerializer.getBuffer = getBuffer
